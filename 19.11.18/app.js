7718181476cf057f1aa40382bdd63df175bbba85
Krystyna Vitenko <kristel@cgmade.com>
2018-11-20 10:18:09 +0200

    19.11.18

---
+++ 19.11.18/app.js
@@ -0,0 +1,51 @@
+// for loop
+
+// for (var i = 0; i < 10; i++) {
+//
+//     var range = i >= 0 && i <= 5;
+//
+//     if(range) {
+//         console.log(i);
+//     }
+//
+// }
+
+
+// // while loop
+//
+//
+// var i = 0;
+// while(i <= 5) {
+//
+//     console.log(i);
+//     i++;
+// }
+
+
+// // do while loop
+//
+//
+// var i = 0;
+//
+// do {
+//     console.log(i);
+//     i++;
+// } while(i <= 5);
+
+
+// function expression
+
+function f() {
+
+    for (var i = 0; i < 10; i++) {
+
+        var range = i >= 0 && i <= 5;
+
+        if (range) {
+            console.log(i);
+        }
+
+    }
+
+}
+
